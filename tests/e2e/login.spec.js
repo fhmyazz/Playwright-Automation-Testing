b9dbb99abf725e9f2a56d84dabdc8eb49a7901d5
fhmyazz <fahmyaziz1805@gmail.com>
2026-02-22 16:12:04 +0700

    separate positive and negative case

--- tests/e2e/login.spec.js
+++ tests/e2e/login.spec.js
@@ -1,7 +1,7 @@
 import {test, expect} from '@playwright/test'
 import { LoginPage } from '../pages/LoginPage.js'
 
-test.describe('Login Page', () => {
+test.describe('Login Page - Positive Case', () => {
     let loginPage
 
     test.beforeEach(async ({page}) => {
@@ -25,6 +25,31 @@ test.describe('Login Page', () => {
         expect(page.url()).toContain('posts.html')
     })
 
+    test('check if login button disable after clicked', async ({page}) => {
+        await page.fill('#username', 'admin')
+        await page.fill('#password', 'admin123')
+
+        // click button without waiting for navigation
+        // if it's not in var, it'll wait to nav
+        const loginPromise = await page.click('#login-button')
+
+        // check disabled button
+        await expect(page.locator('#login-button')).toBeDisabled()
+
+        // now nav after login button
+        await loginPromise
+        await page.waitForURL('**/posts.html')
+    })
+})
+
+test.describe('Login Page - Negative Case', () => {
+    let loginPage
+
+    test.beforeEach(async ({page}) => {
+        loginPage = new LoginPage(page)
+        await loginPage.goto()
+    })
+
     test('should show error with empty username', async ({page}) => {
         const password = 'admin123'
 
@@ -43,34 +68,19 @@ test.describe('Login Page', () => {
         expect(errorText).toBe('Please fill all fields')
     })
 
-    test('check if login button disable after clicked', async ({page}) => {
-        await page.fill('#username', 'admin')
-        await page.fill('#password', 'admin123')
-
-        // click button without waiting for navigation
-        // if it's not in var, it'll wait to nav
-        const loginPromise = await page.click('#login-button')
-
-        // check disabled button
-        await expect(page.locator('#login-button')).toBeDisabled()
-
-        // now nav after login button
-        await loginPromise
-        await page.waitForURL('**/posts.html')
-    })
-
     test('check login using non-existing user', async ({page}) => {
         const username = 'admin'
         const password = 'wrongpassword'
-
+    
         await loginPage.login(username, password)
-
+    
         const isErrorVisible = await loginPage.isVisible('#error-message')
         expect(isErrorVisible).toBe(true) 
-
+    
         const errorText = await loginPage.getErrorText()
         expect(errorText).toMatch(/user not found/i)
-
+    
         expect(page.url()).toContain('login.html')
     })
+
 })
